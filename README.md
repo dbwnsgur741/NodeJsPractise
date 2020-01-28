c69d9b38ca0e2aaa3926dc6a86f39399dd1d175d
Ubuntu <ubuntu@ip-172-31-28-8.us-east-2.compute.internal>
2020-01-28 03:21:03 +0000

    Make ReadMe

---
+++ README.md
@@ -0,0 +1,19 @@
+## NodeJs Practise
+- Node --version ( 12.14.1 )
+- AWS EC2 Ubuntu ( 18.04 )
+- NPM --version ( 6.13.4 )
+
+## Make In basic state
+
+- Download Node Js For your Directory
+- npm init => Make Package.json
+- express --save => For routing server & get static files
+- body-parser => For get request for Post Data
+
+## App.js
+
+- app.listen(3000) => Open Server Port Name 3000
+
+- GET => Get request for req
+
+- POST => Use body-parser
---
+++ app.js
@@ -0,0 +1,40 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var app = express()
+
+
+
+// 정적인 파일들이 있는 곳을 지정해줌 express.static ( .js / .css 등등 )
+app.use(express.static('public'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+app.set('view engine','ejs') // ejs모듈 씀
+
+// 비동기 처리 콜백함수 --> 아래 요청들을 모두 완료한 후에 불러짐 !
+
+app.listen(3000,()=>{
+    console.log("!!!!!!!!!");
+});
+
+// get 요청
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname +'/public/main.html');
+})
+
+app.get('/main',(req,res)=>{
+    res.sendFile(__dirname +'/public/main.html');
+})
+
+app.post('/email_post',(req,res)=>{
+    //get요청일때 --> res.send( req.param('email') ) ;
+   //res.send("<h1> 환영합니다 "+req.body.email+ "님 !!</h1>");
+   res.render('email.ejs',{'email' : req.body.email })
+});
+
+app.post('/ajax_send_email',(req,res)=>{
+
+    //check validation about input value
+
+    var responseData = { 'result' : 'ok' , 'email' : req.body.email }
+    res.json(responseData)
+})
